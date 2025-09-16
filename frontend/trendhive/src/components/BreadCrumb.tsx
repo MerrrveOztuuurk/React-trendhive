0268ffed47f569397cb1fcc257502368048888e3
MerrrveOztuuurk <merrrve.oztuuurk@gmail.com>
2025-09-16 15:39:57 +0300

    Categories component created.

--- frontend/trendhive/src/components/BreadCrumb.tsx
+++ frontend/trendhive/src/components/BreadCrumb.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const BreadCrumb = () => {
   return (
-    <div>BreadCrumb</div>
+    <div className='flex gap-2 px-4 py-4'>
+      <span className='text-gary-500'>Home</span>
+      <span className='text-gary-500'>/</span>
+      <span>Shop</span>
+    </div>
   )
 }
 
